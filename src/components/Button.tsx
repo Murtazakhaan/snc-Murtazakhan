a82656f283a8d2582d6fd350937d229ec8eb5605
Murtazakhaan <murtazakhan5766@gmail.com>
2024-08-17 20:28:16 +0500

    [Frontend] Requirement 1

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,24 @@
 import { FunctionComponent, PropsWithChildren } from "react";
 import classNames from "classnames";
 
-type ButtonProps = {};
+type ButtonProps = {
+  active: boolean | string;
+  onClick: () => void;
+};
 
 export const Button: FunctionComponent<PropsWithChildren<ButtonProps>> = ({
   children,
+  active,
+  onClick,
 }) => {
   return (
     <button
       type="button"
-      className={classNames("px-2 py-1 border border-black")}
+      onClick={onClick}
+      className={classNames(
+        "px-2 py-1 border border-black dark:border-white transition-all",
+        active ? "text-white dark:text-black dark:bg-white bg-black" : "border-black"
+      )}
     >
       {children}
     </button>
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, PropsWithChildren } from "react";
+import { FunctionComponent, PropsWithChildren, useState } from "react";
 import { Inter } from "next/font/google";
 import classNames from "classnames";
 import { Button } from "@/components/Button";
@@ -11,17 +11,24 @@ type MainLayoutProps = {};
 export const MainLayout: FunctionComponent<
   PropsWithChildren<MainLayoutProps>
 > = () => {
+  const [activePerson, setAcitvePerson] = useState("");
   return (
     <main
       className={classNames(
         inter.className,
         "h-screen w-screen",
-        "flex flex-col justify-center items-center",
+        "flex flex-col justify-center items-center"
       )}
     >
       <div className={classNames("flex gap-2")}>
         {Object.values(Person).map((person) => (
-          <Button key={person}>{person}</Button>
+          <Button
+            key={person}
+            active={activePerson === person}
+            onClick={() => setAcitvePerson(person)}
+          >
+            {person}
+          </Button>
         ))}
         <button />
       </div>

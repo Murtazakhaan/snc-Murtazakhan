22242128287acc0320f918419318442b8e92791b
Murtazakhaan <murtazakhan5766@gmail.com>
2024-08-18 17:11:00 +0500

    change: now getPersonFromDB will use db data instead mock data

--- src/utils/server/db.ts
+++ src/utils/server/db.ts
@@ -1,6 +1,8 @@
 import { Person } from "@/utils/common/person";
-import { mockUsers } from "@/utils/server/mock-users";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const getPersonFromDB = async (person: Person) => {
-  return mockUsers[person];
+  return await prisma.user.findFirst({ where : { name : person}});
 };
